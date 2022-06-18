2fb03e47ad4682d111432e47a4e270294377882f
vilher <viktolim@gmail.com>
2022-06-18 23:45:56 +0300

    cleaner code

--- my-app/src/Dictionary.jsx
+++ my-app/src/Dictionary.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import Entry from './Entry';
 import emojiDao from './emojiDao.js';
 
-function getEmojiDao(props){
-    return(
-    <Entry 
-        key={props.id}
-        emoji={props.emoji}
-        info= {props.meaning}
-         name= {props.name}
-    />
-    );
-};
+
 
 function Dictionary(){
-    return <div className="dictionary">
-    {emojiDao.map(getEmojiDao)}
+    return (<div className="dictionary">
+    {emojiDao.map(props =>(
+    <Entry key={props.id} emoji={props.emoji} info= {props.meaning} name= {props.name}/>
+    ))}
     </div>
+    )
 }
 
 export default Dictionary;
